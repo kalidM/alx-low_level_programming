36c9424b885b756800f7ffaaf7f7b13affdf7ed9
KalidM <Kalidmubarek@gmail.com>
2022-12-09 01:15:38 -0800

    yes

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+# include <srdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main-Entry point
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{       printf("size of a char : %i byte (s)\n", sizeof(char));
+	printf("size of an int: %i byte (s) \n", sizeof(int));
+	printf("Size of a long int: %i byte (s) \n", sizeof(long int));
+	printf("Size of a long long int: %i byte (s)\n", sizeof(long long int));
+	printf("size of a float: %i byte (s) \n", sizeof(float));
+	return (0);
+}
