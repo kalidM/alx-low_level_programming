9ed8d279409f3c4dd798cd67b40d6b590a12cc13
KalidM <Kalidmubarek@gmail.com>
2022-12-12 23:01:44 -0800

    yes

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - entry point
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+    char c = 'a';
+
+    while (c <= 'z')
+    {
+	    -putchar(c);
+	    c++;
+    }
+    _putchar('\n');
+
+}
