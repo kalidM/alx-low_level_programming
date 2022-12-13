b4eb14163263a2e3daea99c1a6411390fba289a5
KalidM <Kalidmubarek@gmail.com>
2022-12-13 03:33:24 -0800

    yes

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  *times table - print the 9 times table
+*Return: 0 (Always Success)
+*/
+void times_table(void)
+{
+int a, b, c;
+for (a = 0; a <= 9; at+)
+{
+for (b = o b <= 93 b++)
+C = a * b;
+if ((c / 10) == 0)
+{
+if (b == o)
+putchar(e');
+if (b l= )
+_putchar (
+putchar( (c 10) + *0')
+if (b < 9)
+putchar(
+putchar(
+Ise
